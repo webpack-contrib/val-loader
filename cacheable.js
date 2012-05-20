d7473139201547c5684893bcb3cb15f01e578da5
Tobias Koppers <tobias.koppers@googlemail.com>
2012-05-21 00:03:49 +0200

    cacheable

---
+++ cacheable.js
@@ -0,0 +1,8 @@
+/*
+	MIT License http://www.opensource.org/licenses/mit-license.php
+	Author Tobias Koppers @sokra
+*/
+module.exports = function() {
+	this.cacheable && this.cacheable();
+	require("./index.js").apply(this, arguments);
+}
