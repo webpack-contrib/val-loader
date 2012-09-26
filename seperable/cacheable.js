5379241bdde0aa2f5449128bd33281b0f65a2648
Tobias Koppers <tobias.koppers@googlemail.com>
2012-09-26 13:29:40 +0200

    offer seperable versions

---
+++ seperable/cacheable.js
@@ -0,0 +1,9 @@
+/*
+	MIT License http://www.opensource.org/licenses/mit-license.php
+	Author Tobias Koppers @sokra
+*/
+module.exports = function() {
+	this.cacheable && this.cacheable();
+	require("./index.js").apply(this, arguments);
+}
+module.exports.seperable = true;
