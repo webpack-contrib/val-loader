03d9dcd263a1fe2ad666cc5cebf943eeb29e29e7
Tobias Koppers <tobias.koppers@googlemail.com>
2013-01-30 18:02:39 +0100

    fix for webpack 0.9

--- index.js
+++ index.js
@@ -3,19 +3,13 @@
 	Author Tobias Koppers @sokra
 */
 var loaderUtils = require("loader-utils");
-module.exports = function() {
+module.exports = function(content) {
 	var query = loaderUtils.parseQuery(this.query);
 	if(query.cacheable && this.cacheable)
 		this.cacheable();
-	if(this.inputValues) {
-		var arr = Array.prototype.slice.call(this.inputValues, 0);
-		arr.unshift(null);
-		this.callback.apply(null, arr);
+	if(this.inputValue) {
+		return null, this.inputValue;
 	} else {
-		var arr = Array.prototype.slice.call(arguments, 0);
-		for(var i = 0, l = arr.length; i < l; i++)
-			arr[i] = this.exec(arr[i], this.resource);
-		arr.unshift(null);
-		this.callback.apply(null, arr);
+		return this.exec(content, this.request);
 	}
 }
