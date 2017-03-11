9f70d4b233f8aa55626ac850aad27e0e4dcd6cd9
Johannes Ewald <johannes.ewald@peerigon.com>
2017-03-11 02:39:28 +0100

    Remove separable loader
    This feature is not available in webpack for a long time now.

--- separable/index.js
+++
@@ -1,9 +0,0 @@
-/*
-	MIT License http://www.opensource.org/licenses/mit-license.php
-	Author Tobias Koppers @sokra
-*/
-var mainLoader = require("../index");
-module.exports = function() {
-	return mainLoader.apply(this, arguments);
-}
-module.exports.separable = true;
