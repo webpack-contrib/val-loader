50fe5d435982457feec159c5be40d397242da6f5
Evilebot Tnawi <evilebottnawi@users.noreply.github.com>
2019-11-25 18:44:18 +0300

    refactor: code (#45)

--- test/helpers/index.js
+++ test/helpers/index.js
@@ -1,7 +1,15 @@
 import compile from './compile';
+import execute from './execute';
 import getCompiler from './getCompiler';
 import helperLoader from './helperLoader';
 import normalizeErrors from './normalizeErrors';
 import readAsset from './readAsset';
 
-export { compile, getCompiler, helperLoader, normalizeErrors, readAsset };
+export {
+  compile,
+  execute,
+  getCompiler,
+  helperLoader,
+  normalizeErrors,
+  readAsset,
+};
